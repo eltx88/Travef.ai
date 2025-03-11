3af1c28e71eb72fe93f32deabe47ae386fac3bd6
eltx88 <euanliew77@gmail.com>
2025-03-11 22:11:03 +0000

    Final ui CHANGES

--- frontend/src/components/Homepage/TripCard.tsx
+++ frontend/src/components/Homepage/TripCard.tsx
@@ -52,7 +52,7 @@ const TripCard: FC<TripCardsProps> = ({ trip, onDelete, setGlobalLoading }) => {
     );
     
     return matchedCity?.image_link || 
-      "https://images.unsplash.com/photo-1548800687-96dc03940478?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+      "https://images.unsplash.com/photo-1508939546992-1252a12b4299?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   };
 
   return (
